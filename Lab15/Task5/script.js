9b624f778dd40413868655cf9b8d9fa283ded6ed
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-04-24 21:47:02 -0500

    Do Lab 15 Task 5

---
+++ Lab15/Task5/script.js
@@ -0,0 +1,18 @@
+function updateCountdown() {
+    const currentTime = new Date();
+    const newYear = new Date(currentTime.getFullYear() + 1, 0, 1);
+    const timeDifference = newYear - currentTime;
+
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    //Вирішив не виділяти додаткові змінні, а просто скористатися тернарним оператором
+    document.getElementById('days').textContent = days < 10 ? '0' + days : days;
+    document.getElementById('hours').textContent = hours < 10 ? '0' + hours : hours;
+    document.getElementById('minutes').textContent = minutes < 10 ? '0' + minutes : minutes;
+    document.getElementById('seconds').textContent = seconds < 10 ? '0' + seconds : seconds;
+}
+
+setInterval(updateCountdown, 1000);
