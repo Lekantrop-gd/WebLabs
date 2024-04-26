c37bd893b52239dea5e6ce32c8b55655ba3d6931
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-04-25 20:51:11 -0500

    Do Lab 14 Task 4

---
+++ Lab14/Task4/script.js
@@ -0,0 +1,22 @@
+function findUnique(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+console.log(findUnique([1, 2, 3, 5, 3]));
+console.log(findUnique([1, 2, 3, 5, 11]));
+
+//Це ще одне завдання, яке можна зробити простішии способом
+function findUnique(arr) {
+    const uniqueSet = new Set(arr);
+    return uniqueSet.size === arr.length;
+}
+
+console.log(findUnique([1, 2, 3, 5, 3]));
+console.log(findUnique([1, 2, 3, 5, 11]));
