ba35f2211791e850ce4021eddf98dea96ef42fb9
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-04-25 20:42:20 -0500

    Update script.js

--- Lab14/Task1/script.js
+++ Lab14/Task1/script.js
@@ -1,3 +1,11 @@
 let cities = ["Velen", "Skellige", "Vizima"];
+
+let citiesString = cities[0];
+for (let i = 1; i < cities.length; i++) {
+    citiesString += "*" + cities[i];
+}
+console.log(citiesString);
+
+//Вирішив ще додати простіший спосіб, не через цикл
 let result = cities.join("*");
 console.log(result);
