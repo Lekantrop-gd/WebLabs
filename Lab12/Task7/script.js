40052c36d3788d743e6cb3cb58a243c9dee55e83
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-04-26 19:53:49 -0500

    Do Lab 12 Task 7

---
+++ Lab12/Task7/script.js
@@ -0,0 +1,5 @@
+var login = prompt("Please input your login:");
+var email = prompt("Please input your email:");
+var password = prompt("Please input your password:");
+
+alert("Dear " + login + ", your email is " + email + ", your password is " + password + ".");
