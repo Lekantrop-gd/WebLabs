8e55614179991fd7bc1efac46b629a2e4e259bba
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-04-26 19:45:03 -0500

    Do Lab 12 Task 6

---
+++ Lab12/Task6/script.js
@@ -0,0 +1,14 @@
+var firstName = "Nazar";
+var lastName = "Holodiuk";
+var group = "KS-321";
+var birthYear = 2006;
+var isMarried = false;
+
+console.log("Number:", birthYear);
+console.log("Boolean:", isMarried);
+console.log("String:", firstName, lastName, group);
+
+var nullVariable = null;
+var undefinedVariable;
+console.log("Type of nullVariable:", typeof nullVariable);
+console.log("Type of undefinedVariable:", typeof undefinedVariable);
