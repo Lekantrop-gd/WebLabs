883fc4da0039aeb06b2452d9c296982e1ec0ecec
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-04-26 19:57:52 -0500

    Do Lab 12 Task 8

---
+++ Lab12/Task8/script.js
@@ -0,0 +1,7 @@
+var secondsInHour = 60 * 60;
+var secondsInDay = secondsInHour * 24;
+var secondsInMonth = secondsInDay * 30; // Припущення: місяць має 30 днів. Загалом, не впевнений, чи правильно зрозумів слово "Вирахувати"
+
+console.log("Number of seconds in an hour: ", secondsInHour);
+console.log("Number of seconds in a day: ", secondsInDay);
+console.log("Number of seconds in a month (assumption): ", secondsInMonth);
