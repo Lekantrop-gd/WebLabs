e78ccb4de5332ed98488b6fdf3c6ffc51eed3340
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-04-26 19:26:50 -0500

    Do Lab 12 Task 3

---
+++ Lab12/Task3/script.js
@@ -0,0 +1,12 @@
+var firstVariable, secondVariable;
+
+firstVariable = 10;
+secondVariable = "Hello";
+
+console.log("The value of the first variable: ", firstVariable);
+console.log("The value of the second variable: ", secondVariable);
+
+firstVariable = secondVariable;
+
+console.log("The value of the first variable after copying: ", firstVariable);
+console.log("The value of the second variable after copying: ", secondVariable);
