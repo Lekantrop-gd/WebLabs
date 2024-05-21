0fd1e0446b2f5054769a226067dc634b9ee7968f
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-05-20 23:24:00 -0500

    Do Lab 23

---
+++ Lab23/scripts/card-renderer.js
@@ -0,0 +1,54 @@
+function renderProductCard(product) {
+    var card = document.createElement('div');
+    card.classList.add('product-card');
+
+    var discount = document.createElement('p');
+    discount.classList.add('product-discount');
+    discount.textContent = product.discountPercentage + '%';
+    card.appendChild(discount);
+
+    var image = document.createElement('div');
+    image.classList.add('product-image');
+    image.style.backgroundImage = `url(${product.thumbnail})`;
+    card.appendChild(image);
+
+    var category = document.createElement('p');
+    category.classList.add('product-category');
+    category.textContent = product.category;
+    card.appendChild(category);
+
+    var title = document.createElement('h1');
+    title.classList.add('product-title');
+    title.textContent = product.title;
+    card.appendChild(title);
+
+    var description = document.createElement('p');
+    description.classList.add('product-description');
+    description.textContent = product.description;
+    card.appendChild(description);
+
+    var footer = document.createElement('div');
+    footer.classList.add('product-footer');
+    card.appendChild(footer);
+
+    var price = document.createElement('div');
+    price.classList.add('product-price');
+    price.textContent = `$${product.price}`;
+    footer.appendChild(price);
+
+    var buttons = document.createElement('div');
+    buttons.classList.add('product-buttons');
+    footer.appendChild(buttons);
+    
+    var cart = document.createElement('div');
+    cart.classList.add('product-cart');
+    cart.setAttribute('onclick', `editProduct('${product.title}')`);
+    buttons.appendChild(cart);
+
+    var bin = document.createElement('div');
+    bin.classList.add('product-bin');
+    bin.setAttribute('onclick', `deleteProduct('${product.title}')`);
+    buttons.appendChild(bin);
+
+    return card;
+}
