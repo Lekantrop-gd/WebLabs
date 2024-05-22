f245aa8ab3cfe47c6871da0e7c2066cead090fb5
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-05-22 18:58:34 -0500

    Do Lab 24 Task 5

---
+++ Lab24/Task5/scripts/card-renderer.js
@@ -0,0 +1,35 @@
+function renderProductCard(product) {
+    var $card = $('<div>').addClass('product-card');
+
+    var $discount = $('<p>').addClass('product-discount').text(product.discountPercentage + '%');
+    $card.append($discount);
+
+    var $image = $('<div>').addClass('product-image').css('background-image', `url(${product.thumbnail})`);
+    $card.append($image);
+
+    var $category = $('<p>').addClass('product-category').text(product.category);
+    $card.append($category);
+
+    var $title = $('<h1>').addClass('product-title').text(product.title);
+    $card.append($title);
+
+    var $description = $('<p>').addClass('product-description').text(product.description);
+    $card.append($description);
+
+    var $footer = $('<div>').addClass('product-footer');
+    $card.append($footer);
+
+    var $price = $('<div>').addClass('product-price').text(`$${product.price}`);
+    $footer.append($price);
+
+    var $buttons = $('<div>').addClass('product-buttons');
+    $footer.append($buttons);
+
+    var $cart = $('<div>').addClass('product-cart').attr('onclick', `editProduct('${product.title}')`);
+    $buttons.append($cart);
+
+    var $bin = $('<div>').addClass('product-bin').attr('onclick', `deleteProduct('${product.title}')`);
+    $buttons.append($bin);
+
+    return $card.get(0);
+}
