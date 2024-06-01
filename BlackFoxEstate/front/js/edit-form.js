9bc3acc9e068c9f0047b7b9fd04f8d6b4ce85910
Nazar Holodiuk <98541354+Lekantrop-gd@users.noreply.github.com>
2024-06-01 10:07:12 -0500

    Fix images on form

--- BlackFoxEstate/front/js/edit-form.js
+++ BlackFoxEstate/front/js/edit-form.js
@@ -1,3 +1,5 @@
+window.addEventListener("DOMContentLoaded", assignFilesListener);
+
 function showEdit() {
     document.getElementById("room-edit").style.display = "flex";
     document.forms["room-edit-form"].reset();
@@ -9,4 +11,24 @@ function hideEdit() {
     var oldElement = document.forms["room-edit-form"];
     var newElement = oldElement.cloneNode(true);
     oldElement.parentNode.replaceChild(newElement, oldElement);
+}
+
+function assignFilesListener() {
+    document.getElementById('files').addEventListener('change', function(event) {
+        const files = event.target.files;
+        const imagesContainer = document.getElementById('images');
+        imagesContainer.innerHTML = '';
+    
+        Array.from(files).forEach(file => {
+            if (file.type.startsWith('image/')) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    const img = document.createElement('img');
+                    img.src = e.target.result;
+                    imagesContainer.appendChild(img);
+                };
+                reader.readAsDataURL(file);
+            }
+        });
+    });
 }
--- BlackFoxEstate/front/js/view-form.js
+++ BlackFoxEstate/front/js/view-form.js
@@ -1,6 +1,6 @@
-function showView() {
+function showView(id) {
     document.getElementById("room-view").style.display = "flex";
-    document.forms["room-view-form"].reset();
+    displayRoom();
 }
 
 function hideView() {
@@ -9,4 +9,29 @@ function hideView() {
     var oldElement = document.forms["room-view-form"];
     var newElement = oldElement.cloneNode(true);
     oldElement.parentNode.replaceChild(newElement, oldElement);
+}
+
+async function displayRoom(id) {
+    const response = await fetch(`/product/find/${productId}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    const data = await response.json();
+
+    fillRoom(data);
+}
+
+function fillRoom(room) {
+    // var images = document["view-form"]["images"];
+    // images.innerHTML = "";
+    // room.images.forEach(function(image) {
+    //     var roomImg = document.createElement('img');
+    //     roomImg.src = "../";
+    //     amenityImg.alt = amenity;
+    //     amenities.appendChild(amenityImg);
+    // });
+    console.log(room);
 }
